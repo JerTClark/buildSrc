cc5d0f27ea7653c74ddb25ee2cea055cf280a797
JerTClark <jercla317@gmail.com>
2016-06-17 15:59:19 -0400

    Add a "no clock" version of Today
    Provides a "to the date" timestamp alternative to the "to the minute"
    timestamp

--- src/main/groovy/timestamp/Today.groovy
+++ src/main/groovy/timestamp/Today.groovy
@@ -5,19 +5,28 @@ import java.text.SimpleDateFormat
 class Today {
 
     private String today
+    private String noClock
 
     public Today() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEMMMddyyyy-kkmm")
+        SimpleDateFormat simpleDateFormatNoClock = new SimpleDateFormat("EEEMMMddyyyy")
         Calendar calendar = Calendar.instance
-        this.today = simpleDateFormat.format(calendar.getTime())
+        Date date = calendar.getTime()
+        this.today = simpleDateFormat.format(date)
+        this.noClock = simpleDateFormatNoClock.format(date)
     }
 
     @Override
     public String toString() {
-        return this.today;
+        this.today;
+    }
+
+    public String noClock() {
+        this.noClock
     }
 
     public static void main(String[] args) {
         println new Today()
+        println new Today().noClock
     }
 }
